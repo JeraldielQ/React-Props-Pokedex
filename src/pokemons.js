292ad885cc07f31c5e19bcff8e0a297791b5c14f
Jeraldiel Quinones <jeraldielquinones@Jeraldiels-iMac.home>
2023-08-13 10:45:22 -0700

    Added the array of pokemons.

---
+++ src/pokemons.js
@@ -0,0 +1,26 @@
+const pokemons = [
+    {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
+    {id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
+    {id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
+    {id: 12,  name: 'Butterfree', type: 'flying',   base_experience: 178},
+    {id: 25,  name: 'Pikachu',    type: 'electric', base_experience: 112},
+    {id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
+    {id: 94,  name: 'Gengar',     type: 'poison',   base_experience: 225},
+    {id: 100, name: 'Voltorb',    type: 'electric', base_experience: 66},
+    {id: 102, name: 'Exeggcute',  type: 'grass',    base_experience: 65},
+    {id: 104, name: 'Cubone',     type: 'ground',   base_experience: 64},
+    {id: 109, name: 'Koffing',    type: 'poison',   base_experience: 68},
+    {id: 111, name: 'Rhyhorn',    type: 'ground',   base_experience: 69},
+    {id: 116, name: 'Horsea',     type: 'water',    base_experience: 59},
+    {id: 120, name: 'Staryu',     type: 'water',    base_experience: 68},
+    {id: 123, name: 'Scyther',    type: 'bug',      base_experience: 100},
+    {id: 126, name: 'Magmar',     type: 'fire',     base_experience: 173},
+    {id: 129, name: 'Magikarp',   type: 'water',    base_experience: 40},
+    {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65},
+    {id: 138, name: 'Omanyte',    type: 'rock',     base_experience: 71},
+    {id: 140, name: 'Kabuto',     type: 'rock',     base_experience: 71},
+    {id: 147, name: 'Dratini',    type: 'dragon',   base_experience: 67},
+];
+
+
+  export {pokemons}
